7b9cab3643847b55438abafe5d2291077be13fff
WesinAlves <wesinec@gmail.com>
2021-02-08 15:57:51 -0300

    add database resources

---
+++ resources/database/variables.tf
@@ -0,0 +1,9 @@
+# ... existing code ...
+
+variable "db_username" {
+  description = "Username for the RDS Postgres instance"
+}
+
+variable "db_password" {
+  description = "Password for the RDS postgres instance"
+}
